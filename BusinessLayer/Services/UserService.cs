f981237d5fc97a5efc9612500112f75d37fa18da
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-11-22 11:53:19 +0530

    hashed the user password

--- BusinessLayer/Services/UserService.cs
+++ BusinessLayer/Services/UserService.cs
@@ -1,9 +1,12 @@
 ﻿using DataAcessLayer.Entities;
 using DataAcessLayer.Interfaces;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Org.BouncyCastle.Crypto.Generators;
 using PracticeProject.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +22,14 @@ namespace BusinessLayer.Services
 
         public void InsertUser(UserSignUp user)
         {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: user.Password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+            user.Password = hashedPassword;
             _userRepository.InsertUser(user);
         }
         public void Save()
